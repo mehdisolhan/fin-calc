bc1380752af3922b98c03fdfdf5f51fc251d6481
mehdisolhan <mehdisolhan@gmail.com>
2024-05-26 13:51:10 +0300

    table added

---
+++ components/AveragePriceCalculator.vue
@@ -0,0 +1,58 @@
+<template>
+  <UTable :rows="rows" :columns="columns">
+    <template #price-data="{ row }">
+      <UInput v-model="row.price" class="border-1" />
+    </template>
+  </UTable>
+</template>
+<script setup>
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+
+const columns = [
+  {
+    key: 'id',
+    label: '#'
+  },
+  {
+    key: 'price',
+    label: t('price')
+  },
+  {
+    key: 'quantity',
+    label: t('lotQuantity')
+  },
+  {
+    key: 'total',
+    label: t('total')
+  }
+]
+
+const rows = [
+  {
+    id: 1,
+    price: 0,
+    quantity: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    price: 0,
+    quantity: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    price: 0,
+    quantity: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    price: 0,
+    quantity: 0,
+    total: 0
+  }
+]
+const defaultRowCount = 4
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,9 @@
 <template>
-  <div class="w-full h-full p-4">{{ $t('content') }}</div>
+  <!-- <div class="w-full h-full p-4">{{ $t('content') }}</div> -->
+  <div class="mx-auto p-4">
+    <h3>{{ $t('averagePriceCalculate') }}</h3>
+    <UDivider class="my-2" />
+    <AveragePriceCalculator />
+  </div>
 </template>
 <script setup></script>
