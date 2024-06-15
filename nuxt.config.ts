b51d1313166e6c880163d9e001ad429a02b799d1
mehdisolhan <mehdisolhan@gmail.com>
2024-06-15 18:45:59 +0300

    some seo things

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Fin-Calc | Hisse Maliyeti Hesaplayıcı',
+      htmlAttrs: {
+        lang: 'tr'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Fin-Calc, hisse maliyeti hesaplayıcı web uygulaması.' },
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content: 'fiyat, hesaplama, hisse, lot, maliyet, ortalama, web uygulaması'
+        },
+        { name: 'robots', content: 'index, follow' }
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    }
+  },
   devtools: { enabled: false },
   plugins: ['~/plugins/i18n/index.js'],
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', 'nuxt-icon'],
-  css: ['@/assets/css/tailwind.css']
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/sitemap', '@nuxtjs/robots'],
+  css: ['@/assets/css/tailwind.css'],
+  site: {
+    url: 'https://fin-calcc.vercel.app/'
+  },
+  robots: {
+    rules: [
+      {
+        UserAgent: '*',
+        Disallow: ''
+      }
+    ],
+    sitemap: 'https://fin-calcc.vercel.app/sitemap.xml'
+  }
 })
